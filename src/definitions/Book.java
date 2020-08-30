301256d21cc62d01663fac3339011a95e0cb12c4
manuraj <Manuraj-Singh-Rathore>
2020-08-30 17:26:05 +0530

    getter and setter created

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -26,4 +26,31 @@ public class Book {
         this.authorName = " Chetan Bhagat .";
         this.thirteenDigitISBNNumber = "1548965239575";
     }
+    public String getBookName() {
+
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+
+        this.bookName = bookName;
+    }
+    public String getAuthorName() {
+
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+
+        this.authorName = authorName;
+    }
+    public String getThirteenDigitISBNNumber() {
+
+        return thirteenDigitISBNNumber;
+    }
+
+    public void setThirteenDigitISBNNumber(String thirteenDigitISBNNumber) {
+
+        this.thirteenDigitISBNNumber = thirteenDigitISBNNumber;
+    }
 }
